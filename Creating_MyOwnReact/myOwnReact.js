fb70c83a14ac532f73c9150ee9570a70d47d78e3
Muhammad Zaid <mzaiduu44@gmail.com>
2025-10-08 16:13:58 +0500

    Refined comments and fixed spellings in customRndr function

--- Creating_MyOwnReact/myOwnReact.js
+++ Creating_MyOwnReact/myOwnReact.js
@@ -1,5 +1,6 @@
 function customRndr(reactEle, recievedElement) {
   const domlement = document.createElement(reactEle.type);
+
   // domlement.innerHTML = reactEle.children;
   // domlement.setAttribute("href", reactEle.props.href);
   // domlement.setAttribute("target", reactEle.props.target);
@@ -7,12 +8,16 @@ function customRndr(reactEle, recievedElement) {
   // console.log(recievedElement);
   // recievedElement.appendChild(domlement);
 
+  // Assign children to the created DOM element
   domlement.innerHTML = reactEle.children;
+
+  // Loop through all props and set them as attributes (except 'children')
   for (let prop in reactEle.props) {
-    if (prop == "children") continue;
+    if (prop === "children") continue;
     domlement.setAttribute(prop, reactEle.props[prop]);
     console.log(reactEle.props[prop]);
 
+    // Append the created element to the received container
     recievedElement.appendChild(domlement);
   }
 }
@@ -25,9 +30,14 @@ const createElement = {
   },
   children: "Click here to visit Google",
 };
+
 const container = document.getElementById("root");
 
 customRndr(createElement, container);
 
-//Final Conclusion:- React has own kys to rendr any elment that means there pr defined syntax that it expct from us
-// I have created my own custom rnder which is rendering "a" tag but in vite react app it will not excut czo of our custom defined keys. If i want to render it i just have pass th values without keys.
+// Final Conclusion:
+// React has its own keys and mechanisms to render elements,
+// which means it expects a predefined syntax and structure from us.
+// I have created my own custom render function that is rendering an "a" tag,
+// but in a Vite React app, this will not execute because React uses its own rendering logic.
+// If I want to render it manually, I would need to pass the values directly without using custom-defined keys.
